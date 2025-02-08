a4c4dc9afe466346af098e690044af92af76b377
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-02-08 11:43:23 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,40 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
+
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " kashish"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // if string is written first then every other no is converted to string
+// console.log(1 + 2 + "2"); // if string is not first then operation is performed then string 
+
+// console.log(true);
+// console.log(+true); //+ sign will convert it to number
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++        read postfix and prefix difference
+++gameCounter
+console.log(gameCounter);
