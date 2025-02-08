a190826ef9104a7b95ea6e4a7a68503987b30a22
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-02-08 07:33:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser
+// to make code readable write code in seperate lines semicolon is not important in JS
+
+let name = "Kashish"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+//primitive datatypes
+// number => 2 to power 53
+// bigint
+// string => "" prefer double quotes
+// boolean => true/false
+// null => standalone value (empty)
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Kashish");
+console.log(typeof age);
+console.log(typeof null);   // null ek tarah ka object hai (interview ques)
+console.log(typeof undefined); // output is undefined
