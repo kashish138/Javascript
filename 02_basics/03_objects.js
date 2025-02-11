d51a53ce7110d36f15db7981dc179fff42818be2
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-02-11 20:10:19 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// objects can be declared in two ways : literal and as constructor
+// objects are defined in key value pairs
+// singleton  - not in case of literals
+// Object.create 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Kashish",
+    "full name": "Kashish Bhatia",
+    mySym: "mykey1",
+    age: 20,
+    location: "Faridabad",
+    email: "kashishb@gmai.com",
+    isLoggedIn: false,
+    lastLogginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email) // not a right way to print
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser.mySym)
+
+JsUser.email = "kashishb25@gmail.com"
+// Object.freeze(JsUser)     // freeze ke baad kuch change ni hoga
+JsUser.email = "kashishg@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+console.log(JsUser.greeting());
