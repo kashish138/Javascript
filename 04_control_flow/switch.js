4b5670f8a74afd8858b7a1096d4d4c0a2df7ed4b
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-02-17 09:56:30 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {  // shift + alt + down arrow se duplicate hota
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case "may":
+        console.log("May")
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
