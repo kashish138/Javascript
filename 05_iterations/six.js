4259bb87d07635ad34c2992e5aa11ee5871d7585
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-02-18 17:28:31 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,29 @@
+// filter
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let newNums = myNums.filter( (num) => num > 4)  // filter mein condition di jaati hai
+// console.log(newNums);
+
+// let newNums = myNums.filter( (num) => {   // due to this scope we need to write return otherwise it will not print anything
+//     return num > 4
+// })
+
+// const newNums = []    
+
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
