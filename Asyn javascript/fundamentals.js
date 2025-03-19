d1d785e6d8672e57c79feaf08d5dde8252bd5754
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-03-19 16:20:56 +0000

    fundamentals

---
+++ Asyn javascript/fundamentals.js
@@ -0,0 +1,15 @@
+// Javascript - synchronous and single threaded
+// synchronous mane ek code ke baad next code execute hoga like this
+// single threaded mane ek thread pr hi hoti hai yes it is slow 
+
+// Execution context - execute one line of code at a time
+// each operation waits for the last one to complete before executing
+
+// Blocking code vs Non Blocking code
+// Blocking - Block the flow of program , Read file sync
+// Non blocking - Read file Async
+
+// JS Engine is madee up of memory heap and call stack
+// Web API (inside browser) - DOM acess
+// task queue - add to call stack
+// new api - fetch api = high priority baki same function
