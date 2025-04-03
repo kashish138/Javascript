d01b1ee01cb834970985885f6cddc4155cbc89df
kashish138 <146374962+kashish138@users.noreply.github.com>
2025-04-03 18:38:48 +0000

    notes closure

---
+++ closure_and_lexical_scoping/closure.js
@@ -0,0 +1,6 @@
+// A closure is the combination of a function enclosed with references to its surrounding state(lexical environment)
+
+// A closure is created every time a function is created. It gives you access to an outer functions scope from an inner function
+
+// Lexical Scoping
+// init() creates a local variable called name and a function called displayName(). DisplayName() function is an inner function that is defined inside init() and is available only within the body of the init() function
